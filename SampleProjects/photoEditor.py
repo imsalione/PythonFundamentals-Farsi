89ed8dd6cccb0e7c7ecffcbc10ba92ef41ffc70f
Saleh Abedinezhad <42279136+imsalione@users.noreply.github.com>
2024-01-11 00:20:39 +0330

    new sample added

---
+++ SampleProjects/photoEditor.py
@@ -0,0 +1,14 @@
+from PIL import Image, ImageEnhance, ImageFilter
+import os
+
+path = './images'
+pathOut = '/editedImage'
+
+for filename in os.listdir(path):
+    img = Image.open(f"{path}/{filename}")
+    
+    edit = img.filter(ImageFilter.SHARPEN).convert('L')
+    
+    clean_name = os.path.splitext(filename)[0]
+    
+    edit.save(f'.{pathOut}/{clean_name}_edited.jpg')
