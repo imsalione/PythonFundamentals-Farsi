01d2fb2f8614c42e0a64b0b462218fcdb5cd72ce
imsalione <salehabedinezhad@gmail.com>
2024-02-18 14:46:09 +0330

    new changes

--- w3schools/src/sample.py
+++ w3schools/src/sample.py
@@ -4,7 +4,7 @@ while True:
     try:
         a = int(input("Enter number 1: "))
         b = int(input("Enter number 2: "))
-        print("The sum is: ", a + b)
+        print("Result:", a + b)
         sys.exit()
     except ValueError:
         print("You shoud enter an integer number!")
