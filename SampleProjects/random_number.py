b77765350603dac48037797e92294ed2bee88ad4
imsalione <salehabedinezhad@gmail.com>
2024-03-01 23:47:15 +0330

    new sample added

---
+++ SampleProjects/random_number.py
@@ -0,0 +1,6 @@
+import random
+
+start_number = int(input('Enter start number: '))
+end_number = int(input('Enter end number: '))
+
+print('Random number: ', random.randint(start_number, end_number))
---
+++ SampleProjects/square_root.py
@@ -0,0 +1,5 @@
+num = int(input('Enter a number: '))
+
+num_sqr = num ** 0.5
+
+print('The square root of number is: ', num_sqr)
