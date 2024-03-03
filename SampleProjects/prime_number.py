71fde0e0b55734189327df8c33ee9a97b8866fb0
imsalione <salehabedinezhad@gmail.com>
2024-03-03 16:28:33 +0330

    new sample added

---
+++ SampleProjects/prime_number.py
@@ -0,0 +1,18 @@
+# Program to chech if a number is prime or not
+
+num = int(input('Enter a number: '))
+
+flag = False
+
+if num == 1:
+    print(num, 'is not a prime number')
+elif num > 1:
+    for i in range(2, num):
+        if (num % i) == 0:
+            flag = True
+            break
+
+if flag:
+    print(num, 'is not a prime number')
+else:
+    print(num, 'is a prime number')
