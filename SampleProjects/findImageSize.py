f73eb948698506ec1bc6f814471d3aa0ffb1c451
imsalione <salehabedinezhad@gmail.com>
2024-03-25 22:52:53 +0330

    new sample

---
+++ SampleProjects/findImageSize.py
@@ -0,0 +1,16 @@
+def jepg_res(filename):
+    with open(filename,'rb') as img_file:
+        
+        img_file.seek(163)
+        
+        a = img_file.read(2)
+        
+        height = (a[0] << 8) + a[1]
+        
+        a = img_file.read(2)
+        
+        width = (a[0] << 8) + a[1]
+        
+    print("The resolution of the image is", width, "x", height)
+    
+jepg_res("512kb.jpg")
