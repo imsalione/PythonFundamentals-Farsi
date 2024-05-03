408f7b02cf4b97db9875491a718f092d17077c89
imsalione <salehabedinezhad@gmail.com>
2024-05-03 23:52:09 +0330

    new changes

--- SampleProjects/concatenateTwolists.py
+++ SampleProjects/concatenateTwolists.py
@@ -4,3 +4,27 @@ list2 = [3, 4, 5]
 list_joined = list1 + list2
 
 print(list_joined)
+
+print('--------------------------')
+
+list3 = [1, 'a']
+list4 = range(3, 6)
+
+list_joined = [*list3, *list4]
+print(list_joined)
+
+print('--------------------------')
+
+list5 = [1, 'a']
+list6 = [3, 4, 5]
+
+list_joined = list(set(list5 + list6))
+print(list_joined)
+
+print('--------------------------')
+
+list7 = [1, 'a']
+list8 = [1, 2, 3]
+
+list8.extend(list7)
+print(list8)
