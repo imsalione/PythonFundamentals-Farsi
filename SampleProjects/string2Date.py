3ca1ab5b728276a0012c0fcf579220091fa45c63
imsalione <salehabedinezhad@gmail.com>
2024-05-07 22:54:10 +0330

    new changes

--- SampleProjects/string2Date.py
+++ SampleProjects/string2Date.py
@@ -1,8 +1,16 @@
 from datetime import datetime
 
-my_data_string = "Mar 11 2024 10:45PM"
+my_data_string = "Feb 25 1988 10:45PM"
 
 datetime_object = datetime.strptime(my_data_string, '%b %d %Y %I:%M%p')
 
 print(type(datetime_object))
 print(datetime_object)
+
+print('--------------------------')
+
+from dateutil import parser
+
+date_time = parser.parse('Feb 25 1988 10:53AM')
+
+print(date_time)
