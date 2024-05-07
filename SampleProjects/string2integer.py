d4f745d4fa3b0ec53ae504eb2446f6f9a19e4b03
imsalione <salehabedinezhad@gmail.com>
2024-05-07 18:59:30 +0330

    new changes

---
+++ SampleProjects/string2integer.py
@@ -0,0 +1,13 @@
+balance_str = "1500"
+balance_int = int(balance_str)
+
+print(type(balance_int))
+print(balance_str)
+
+print('------------------------')
+
+balance_str = "1500.4"
+balance_float = float(balance_str)
+
+print(type(balance_float))
+print(balance_float)
