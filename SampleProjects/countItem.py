8e81c6882ac22683027fe55d506f29064ab8e2b9
imsalione <salehabedinezhad@gmail.com>
2024-05-08 20:07:29 +0330

    new changes

--- SampleProjects/countItem.py
+++ SampleProjects/countItem.py
@@ -1,4 +1,4 @@
-selected_item = 's'
+selected_item = 'a'
 freq = ['a','s', 1, 'a', 4,'s', 3,'s', 2, 'a', 's'].count(selected_item)
 
-print(f"Number of {selected_item}: {freq}")
+print(f"Count of '{selected_item}': {freq}")
