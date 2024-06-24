c98d85f31a87d0bd08d2961d85a28ce184cb3357
imsalione <salehabedinezhad@gmail.com>
2024-06-24 19:01:49 +0330

    django lesson

--- w3schools/40-Django/myworld/my_tennis_club/my_tennis_club/urls.py
+++ w3schools/40-Django/myworld/my_tennis_club/my_tennis_club/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    path('members/', veiws.members, name='members'),
+    path('', include('members.urls')),
+    path('admin/', admin.site.urls),
 ]
