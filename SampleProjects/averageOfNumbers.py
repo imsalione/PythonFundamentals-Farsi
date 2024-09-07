e7d456f217f464f92561564d609e2f7cfa1e1456
imsalione <salehabedinezhad@gmail.com>
2024-09-07 10:50:16 +0330

    new sample

---
+++ SampleProjects/averageOfNumbers.py
@@ -0,0 +1,8 @@
+nums = [1, 2, 3, 4, 5]
+
+total = sum(nums)
+length = len(nums)
+
+average = total / length
+
+print(f"The average of numbers is {average:.2f}")
---
+++ SampleProjects/weightedAverage.py
@@ -0,0 +1,9 @@
+nums = [18.5, 16.0, 15.0, 16.0, 12.0, 19.0, 12.0, 14.5]
+weigths = [3, 2, 3, 2, 2, 1, 3, 3]
+
+weighted_sum = sum(num * weight for num, weight in zip(nums, weigths))
+totla_weight = sum(weigths)
+
+weighted_average = weighted_sum / totla_weight
+
+print(f"The weighted average of numbers is {weighted_average:.2f}")
